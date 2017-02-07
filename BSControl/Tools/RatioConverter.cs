010fb8041d859cf00adc69c6214e3b0179ee3178
greg-sim58 <greg@sim58dev.com>
2017-02-07 17:55:58 +0200

    Initial Commit

---
+++ BSControl/Tools/RatioConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace BSControl.Tools
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class RatioConverter : MarkupExtension, IValueConverter
+    {
+        private static RatioConverter _instance;
+
+        public RatioConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((IValueConverter)_instance).Convert(value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((IValueConverter)_instance).ConvertBack(value, targetType, parameter, culture);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new RatioConverter());
+        }
+
+    }
+}
